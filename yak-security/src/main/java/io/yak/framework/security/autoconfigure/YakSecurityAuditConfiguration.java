b93d5141a76922def4cb61f90018d9739c5a6500
weifuwan <295227940@qq.com>
2026-07-26 13:48:36 +0800

    update

--- yak-security/src/main/java/io/yak/framework/security/autoconfigure/YakSecurityAuditConfiguration.java
+++ yak-security/src/main/java/io/yak/framework/security/autoconfigure/YakSecurityAuditConfiguration.java
@@ -6,8 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(prefix = "yak.security", name = "database-enabled", havingValue = "true", matchIfMissing = true)
-@ConditionalOnProperty(prefix = "yak.security", name = "web-enabled", havingValue = "true", matchIfMissing = true)
-@ConditionalOnProperty(prefix = "yak.security", name = "audit-enabled", havingValue = "true", matchIfMissing = true)
+@ConditionalOnProperty(
+        prefix = "yak.security",
+        name = {
+                "database-enabled",
+                "web-enabled",
+                "audit-enabled"
+        },
+        havingValue = "true",
+        matchIfMissing = true
+)
 @Import(OplogController.class)
-class YakSecurityAuditConfiguration {}
+public class YakSecurityAuditConfiguration {
+}
--- yak-security/src/main/java/io/yak/framework/security/autoconfigure/YakSecurityWebConfiguration.java
+++ yak-security/src/main/java/io/yak/framework/security/autoconfigure/YakSecurityWebConfiguration.java
@@ -1,15 +1,40 @@
 package io.yak.framework.security.autoconfigure;
 
-import io.yak.framework.security.controller.v1.*;
+import io.yak.framework.security.controller.v1.CommonController;
+import io.yak.framework.security.controller.v1.ConfigController;
+import io.yak.framework.security.controller.v1.DeptController;
+import io.yak.framework.security.controller.v1.LoginController;
+import io.yak.framework.security.controller.v1.MessageController;
+import io.yak.framework.security.controller.v1.PermissionController;
+import io.yak.framework.security.controller.v1.ProjectController;
+import io.yak.framework.security.controller.v1.ResourceController;
+import io.yak.framework.security.controller.v1.RoleController;
+import io.yak.framework.security.controller.v1.UserController;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(prefix = "yak.security", name = "database-enabled", havingValue = "true", matchIfMissing = true)
-@ConditionalOnProperty(prefix = "yak.security", name = "web-enabled", havingValue = "true", matchIfMissing = true)
-@Import({CommonController.class, ConfigController.class, DeptController.class,
-    LoginController.class, MessageController.class, PermissionController.class,
-    ProjectController.class, ResourceController.class, RoleController.class,
-    UserController.class})
-class YakSecurityWebConfiguration {}
+@ConditionalOnProperty(
+        prefix = "yak.security",
+        name = {
+                "database-enabled",
+                "web-enabled"
+        },
+        havingValue = "true",
+        matchIfMissing = true
+)
+@Import({
+        CommonController.class,
+        ConfigController.class,
+        DeptController.class,
+        LoginController.class,
+        MessageController.class,
+        PermissionController.class,
+        ProjectController.class,
+        ResourceController.class,
+        RoleController.class,
+        UserController.class
+})
+public class YakSecurityWebConfiguration {
+}
