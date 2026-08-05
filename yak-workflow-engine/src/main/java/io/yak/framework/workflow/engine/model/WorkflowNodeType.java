a8b16e6da6c06df37bf4c0c2a97eeefcb54a317e
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-05 10:33:56 +0800

    feat(workflow): add DAG model

---
+++ yak-workflow-engine/src/main/java/io/yak/framework/workflow/engine/model/WorkflowNodeType.java
@@ -0,0 +1,10 @@
+package io.yak.framework.workflow.engine.model;
+
+/**
+ * Node types supported by the first version of the workflow engine.
+ */
+public enum WorkflowNodeType {
+    START,
+    TASK,
+    END
+}
