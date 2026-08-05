358accec5420dda4a4e362eceef516adcc1c0a72
weifuwan <295227940@qq.com>
2026-08-05 14:31:15 +0800

    update

--- yak-workflow-engine/src/main/java/io/yak/framework/workflow/engine/model/WorkflowEdge.java
+++
@@ -1,9 +0,0 @@
-package io.yak.framework.workflow.engine.model;
-
-/**
- * A directed dependency between two workflow nodes.
- *
- * @param sourceCode upstream node code
- * @param targetCode downstream node code
- */
-public record WorkflowEdge(String sourceCode, String targetCode) {}
