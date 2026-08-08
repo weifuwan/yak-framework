57c9fa5bd6bf12a89e973904c36e1fdf964be3ee
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-08 11:55:03 +0800

    feat(workflow): add pause resume workflow states

--- yak-workflow/yak-workflow-engine/src/main/java/io/yak/framework/workflow/engine/state/WorkflowExecutionStatus.java
+++ yak-workflow/yak-workflow-engine/src/main/java/io/yak/framework/workflow/engine/state/WorkflowExecutionStatus.java
@@ -3,6 +3,9 @@ package io.yak.framework.workflow.engine.state;
 public enum WorkflowExecutionStatus {
     CREATED(false),
     RUNNING(false),
+    PAUSING(false),
+    PAUSED(false),
+    RESUMING(false),
     SUCCESS(true),
     SUCCESS_WITH_WARNINGS(true),
     FAILED(true),
@@ -17,5 +20,9 @@ public enum WorkflowExecutionStatus {
 
     public boolean isTerminal() {
         return terminal;
+    }
+
+    public boolean isPauseLifecycle() {
+        return this == PAUSING || this == PAUSED || this == RESUMING;
     }
 }
