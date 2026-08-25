209cf3674e35423006fe0bcf23982b8a0c0e2bf7
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-25 11:15:31 +0800

    test(security): cover login-state storage defaults

--- yak-security/src/test/java/io/yak/framework/security/config/YakSecurityPropertiesTest.java
+++ yak-security/src/test/java/io/yak/framework/security/config/YakSecurityPropertiesTest.java
@@ -11,7 +11,7 @@ class YakSecurityPropertiesTest {
     YakSecurityProperties properties = new YakSecurityProperties();
 
     assertThat(properties.getDatasource().getDriverClassName())
-        .isEqualTo("com.mysql.cj.jdbc.Driver");
+            .isEqualTo("com.mysql.cj.jdbc.Driver");
   }
 
   @Test
@@ -21,5 +21,22 @@ class YakSecurityPropertiesTest {
     assertThat(properties.getAuthentication().getMode())
             .isEqualTo(
                     YakSecurityProperties.AuthenticationMode.SESSION);
+  }
+
+  @Test
+  void shouldKeepLoginStateInMemoryUnlessRedisIsExplicitlyEnabled() {
+    YakSecurityProperties properties = new YakSecurityProperties();
+
+    assertThat(properties.getAuthentication().getStorage())
+            .isEqualTo(
+                    YakSecurityProperties.AuthenticationStorage.MEMORY);
+    assertThat(properties.getAuthentication().getRedis().getHost())
+            .isEqualTo("127.0.0.1");
+    assertThat(properties.getAuthentication().getRedis().getPort())
+            .isEqualTo(6379);
+    assertThat(properties.getAuthentication().getRedis().getDatabase())
+            .isZero();
+    assertThat(properties.getAuthentication().getRedis().getMaxTotal())
+            .isEqualTo(64);
   }
 }
